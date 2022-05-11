2170cd0b3d4f585c5aea00b4c84523605b38c0ec
Jen Looper <jen.looper@gmail.com>
2022-05-10 20:25:06 -0400

    404

--- docs/.vuepress/theme/index.ts
+++ docs/.vuepress/theme/index.ts
@@ -10,6 +10,7 @@ export const cs4kidsTheme = (options: DefaultThemeOptions): Theme => {
     extends: defaultTheme(options),
     layouts: {
       Layout: path.resolve(__dirname, 'layouts/Layout.vue'),
+      404: path.resolve(__dirname, 'layouts/404.vue'),
       KidsLayout: path.resolve(__dirname, 'layouts/KidsLayout.vue'),
       ContactLayout: path.resolve(__dirname, 'layouts/ContactLayout.vue'),
       AuthLayout: path.resolve(__dirname, 'layouts/AuthLayout.vue'),
