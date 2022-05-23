b9451492f200ade50684a7485810b0532bb4df4b
Jen Looper <jen.looper@gmail.com>
2022-05-22 22:40:37 -0400

    statuscode is necessary

--- netlify/functions/passcode.js
+++ netlify/functions/passcode.js
@@ -5,13 +5,13 @@ exports.handler = async (event, context) => {
 
     if (PASSCODE == queryPasscode) {
       return {
-        status: 200,
+        statusCode: 200,
        body: "Passcode correct!",
       };
     }   
     else {
       return { 
-        status: 406,
+        statusCode: 406,
         body: "Passcode invalid",
 
       }
